a558b8ae8ae26d3e63e68d1b06893f5e14650844
arthur-amorim-bs <aamorim@sprintmind.com.br>
2025-01-17 12:29:22 -0300

    creates webserver and get method to see the fullblockchain

--- main.go
+++ main.go
@@ -3,18 +3,30 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 var Blockchain []Block
 
 func main() {
+	godotenv.Load()
 
 	fmt.Println("Creating Genesis Block...")
 	genesisBlock := createGenesisBlock()
 	fmt.Println("Generating blockchain...")
 	Blockchain = append(Blockchain, genesisBlock)
+	fmt.Println("Blockchain ready!")
+
+	run()
 }
 
 type Block struct {
@@ -78,4 +90,38 @@ func replaceChain(blocks []Block) {
 	if len(blocks) > len(Blockchain) {
 		Blockchain = blocks
 	}
+}
+
+func run() error {
+	mux := makeMuxRouter()
+	httpAddr := os.Getenv("ADDR")
+	log.Println("Listening on ", os.Getenv("ADDR"))
+	s := &http.Server{
+		Addr:           ":" + httpAddr,
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func makeMuxRouter() http.Handler {
+	muxRouter := mux.NewRouter()
+	muxRouter.HandleFunc("/", handleGetBlockchain).Methods("GET")
+	return muxRouter
+}
+
+func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
+	bytes, err := json.MarshalIndent(Blockchain, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	io.WriteString(w, string(bytes))
 }
