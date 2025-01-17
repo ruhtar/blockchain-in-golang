397f3ab6a7ff2aa8b183a1f4521a0c82e50392af
arthur-amorim-bs <aamorim@sprintmind.com.br>
2025-01-17 13:50:28 -0300

    changes logic to always try to find the last block on the chain before adding a new block

--- main.go
+++ main.go
@@ -32,13 +32,13 @@ func main() {
 type Block struct {
 	Index     int    // is the position of the data record in the blockchain
 	Timestamp string // is automatically determined and is the time the data is written
-	BPM       int    //beats per minute
+	Data      []byte //beats per minute
 	Hash      string //is a SHA256 identifier representing this data record
 	PrevHash  string //is the SHA256 identifier of the previous record in the chain
 }
 
 func calculateHash(block Block) string {
-	data := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	data := string(block.Index) + block.Timestamp + string(block.Data) + block.PrevHash
 	h := sha256.New()
 	h.Write([]byte(data))
 	hash := h.Sum(nil) //nil because no other data is passed to be added to the end of the calculus
@@ -55,9 +55,10 @@ func createGenesisBlock() Block {
 	return genesisBlock
 }
 
-func createNewBlock(previousBlock Block, bpm int) (Block, error) {
+func createNewBlock(data string) (Block, error) {
 	var newBlock Block
-	newBlock.BPM = bpm
+	previousBlock := Blockchain[len(Blockchain)-1]
+	newBlock.Data = []byte(data)
 	newBlock.Index = previousBlock.Index + 1
 	newBlock.Timestamp = time.Now().Format(time.RFC3339)
 	newBlock.PrevHash = previousBlock.PrevHash
